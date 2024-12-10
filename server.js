151f5c6288bef18b2ba760c45b63c2166fd3bcb6
Sunil Rawat <sunilrawat@Sunils-MacBook-Air.local>
2024-12-10 12:09:03 +0930

    railway

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use("/campaigns", campaignRoutes);
 app.use("/auth", authRoutes); // attaching authentication routes to /auth endpoint
 
 // Start server
-const PORT = process.env.PORT || 5000; // sets port from environment variables or defaults to 5000
+const PORT = process.env.PORT || 4000; // sets port from environment variables or defaults to 5000
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`); // log the URL when server starts
 });
